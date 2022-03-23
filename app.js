6a7805edc7f671dc9e3b1717a56cfcf45597c3f2
Evgy <evgybarabanova@gmail.com>
2022-03-23 10:46:46 +0300

    советы приходят

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 require('dotenv').config();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
 const { Telegraf } = require('telegraf');
+//const Markup = require('telegraf/markup');
 const com = require('./commands')
+const { Advice } = require('random-advice');
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'какой-то незнакомый человек'}!`))
-bot.help((ctx) => ctx.reply(com.commands))
+bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'какой-то незнакомый человек'}!
+Здесь ты можешь узнать рандомный совет, который поможет отвлечься от серых будней 🙃
+Начнем?)`))
+//bot.help((ctx) => ctx.reply(com.commands))
 
-// bot.command('course')
+bot.on('text', async (ctx)=>{
+let data = {};
+data = await Advice.getAdvice(); 
+//data = await Advice.getRaw()
+const formatData = `
+Совет: ${data}
+`
+
+ctx.reply(formatData);
+})
+
 
 bot.launch()
+
+console.log('Бот запущен');
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
--- commands.js
+++ commands.js
@@ -1,5 +1,6 @@
 const commands = `
 /start -Перезапустить бота
 /help - Помощь
+/letsGo - Приступить к поиску рандомных советов
 `
 module.exports.commands = commands
